52fbf1c508c964ba5b35f40baf87bbd0a84d95f7
one piece <646796992@qq.com>
2025-01-22 17:06:54 +0800

    1

--- app/src/main/java/com/baofu/netlib/MainActivity.java
+++ app/src/main/java/com/baofu/netlib/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
 //        viewModel.requestGsonPOST();
 //        viewModel.sendVifyCode("18259462251");
 
-        String url = Base64.encodeToString("http://192.168.0.101".getBytes(), Base64.DEFAULT);
+        String url = Base64.encodeToString("http://yshidquan5.xyz:9898".getBytes(), Base64.DEFAULT);
         String a=new String( NetUtils.encode(url.toCharArray(),3));
 //        a= "]Z4l]ZTy\\5IzgJoyepYn";
         Log.e("asdf","encode:"+a);
